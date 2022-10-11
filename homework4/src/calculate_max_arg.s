862a108602d150c5bf08ba2c90679851067b23b0
Andrew N <34643231+Bopobywek@users.noreply.github.com>
2022-10-11 22:54:07 +0300

    Added homework4

---
+++ homework4/src/calculate_max_arg.s
@@ -0,0 +1,32 @@
+.intel_syntax noprefix
+
+.section .rodata    # /
+format_string:      # |	Форматная строка для вывода искомого числа	
+	.string	"%d\n"  # \ 
+
+.text
+	.globl	main
+	.type	main, @function
+main:
+	push rbp        # /Пролог функции 
+	mov rbp, rsp    # \
+
+	mov	rax, 18446744073709551615    # Максимально допустимое значение 
+                                     # в 64-х разрядном машинном слове
+	mov	r8d, 1      # Счётчик значения аргумента факториала
+.loop:
+	inc r8          # ++r8
+	xor edx, edx    # Обнуляем rdx для корректного умножения, т.к. берутся регистры rdx:rax
+	div	r8          # Делим, частное попадает в rax
+	cmp	rax, r8     # / Если частное больше делителя, выполняем цикл ещё раз 
+	ja	.loop       # \ ja, т.к не учитываем знак числа
+print_result:
+	lea	rdi, format_string[rip] 
+	mov rsi, r8
+	xor	eax, eax
+	call printf@PLT
+
+	xor	eax, eax
+	pop rbp	
+	ret
+
---
+++ homework4/src/factorial.s
@@ -0,0 +1,54 @@
+.intel_syntax noprefix
+
+.section .rodata
+input_format:
+	.string	"%d"
+output_format:
+	.string	"%lld\n"
+
+.section .data
+n: 
+	.long 0
+
+.text
+.globl	factorial	
+.type	factorial, @function
+factorial:
+	push rbp
+	mov rbp, rsp
+
+	mov rax, 1    # result = 1
+	mov rcx, rdi  # i = n
+loop:
+	cmp rcx, 0        # / while (i != 0)
+	je end_factorial  # \
+	mul rcx           # result *= i
+	dec rcx           # --i 
+	jmp loop         
+end_factorial:        # результат возвращается через rax      
+	pop rbp	
+	ret
+
+.globl	main
+.type	main, @function
+main:
+	push rbp
+	mov rbp, rsp
+scan_input_number:
+	lea	rdi, input_format[rip]
+	lea rsi, n[rip]
+	xor	eax, eax
+	call scanf@PLT
+call_factorial:	
+	mov rdi, n[rip]
+	call factorial
+print_result:	
+	lea rdi, output_format[rip]
+	mov rsi, rax
+	xor eax, eax
+	call printf@PLT
+end_main:
+	xor	eax, eax
+	pop rbp
+	ret
+
